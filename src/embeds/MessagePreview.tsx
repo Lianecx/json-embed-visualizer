dda33c05081ac45db0dfd74b37901c819915441c
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-09-23 15:22:49 +0200

    Updated MessagePreviews

--- src/embeds/MessagePreview.tsx
+++ src/embeds/MessagePreview.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
-import {
-    DiscordOptionsContext,
-    DiscordDefaultOptions,
-} from '@discord-message-components/react';
+import { DiscordButtons, DiscordMessage } from '@discord-message-components/react';
 import '@discord-message-components/react/dist/style.css'
 import EmbedPreview, { EmbedPreviewProps } from './EmbedPreview';
+import { ComponentType } from "discord-api-types/v10";
+import ButtonPreview, { ButtonPreviewProps } from './ButtonPreview';
+import SelectMenuPreview, { SelectMenuPreviewProps } from './SelectMenuPreview';
 
 export type MessagePreviewProps = {
     message: {
-        embeds: EmbedPreviewProps['embed'][],
-        // components: ComponentPreviewProps[],
+        embeds?: EmbedPreviewProps['embed'][],
+        components?: (ButtonPreviewProps['button'] | SelectMenuPreviewProps['selectMenu'])[],
+        console?: string,
     },
+    key: string,
 }
 
 class MessagePreview extends React.Component<MessagePreviewProps> {
 
     render() {
         const message = this.props.message;
+        const embeds = message.embeds;
+        const buttons = message.components?.filter(component => ComponentType[component.type as keyof typeof ComponentType] === ComponentType.Button);
+        const selectMenus = message.components?.filter(component => ComponentType[component.type as keyof typeof ComponentType] === ComponentType.SelectMenu);
 
-        return  <DiscordOptionsContext.Provider value={DiscordDefaultOptions}>
-            {
-                message.embeds.map(embed => <EmbedPreview embed={embed} />)
-            }
-        </DiscordOptionsContext.Provider>;
+        return <DiscordMessage profile="mclinker">
+            {/*{embeds?.map((embed, i) => <EmbedPreview key={`${this.props.key}.${i}`} embed={embed} />)}*/}
+            {/*@ts-ignore*/}
+            {/*{buttons ? <DiscordButtons>{buttons?.map((button, i) => <ButtonPreview key={`${this.props.key}.${i}`} button={button} />)}</DiscordButtons> : null}*/}
+            {/*@ts-ignore*/}
+            {/*{selectMenus ? <div>{selectMenus?.map((selectMenu, i) => <SelectMenuPreview key={`${this.props.key}.${i}`} selectMenu={selectMenu} />)}</div> : null}*/}
+        </DiscordMessage>
     }
 }
 
--- src/embeds/MessagePreviewTable.tsx
+++ src/embeds/MessagePreviewTable.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import MessagePreview, { MessagePreviewProps } from './MessagePreview';
+import {
+    DiscordDefaultOptions,
+    DiscordMessages,
+    DiscordOptionsContext,
+} from "@discord-message-components/react";
 
 export type MessagePreviewTableProps = {
-    data: MessagePreviewProps['message'][],
+    data: (MessagePreviewProps['message'] & { key: string })[],
 }
 
 class MessagePreviewTable extends React.Component<MessagePreviewTableProps> {
 
     render() {
-        return <MessagePreview message={this.props.data[1]} />
+        const options = {
+            ...DiscordDefaultOptions,
+            profiles: {
+                mclinker: {
+                    author: 'MC Linker',
+                    avatar: 'https://cdn.discordapp.com/avatars/712759741528408064/6fe20c64903d6d39cd6fa2005fa694ee.png',
+                    roleColor: '#70A02C',
+                    bot: true,
+                },
+            },
+        };
+
+        return <DiscordOptionsContext.Provider options={options} value="preview-table">
+            <DiscordMessages>
+                {this.props.data.map(message => <MessagePreview key={message.key} message={message} />)}
+            </DiscordMessages>
+        </DiscordOptionsContext.Provider>
     }
 }
 
