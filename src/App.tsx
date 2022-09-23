cfea4e9ef86959e10ec3fb4536ecdbfb20f77696
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-09-23 15:22:31 +0200

    Messages are now recursively searched

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
-import MessagePreviewTable from './embeds/MessagePreviewTable';
+import MessagePreviewTable, {MessagePreviewTableProps} from './embeds/MessagePreviewTable';
 import messages from './assets/embeds.json';
 
-function App() {
-    return <div className="App">
-        <MessagePreviewTable data={messages} />
-    </div>
+class App extends React.Component {
+
+    render() {
+
+        //Loop recursively through messages until an object contains keys "embeds", "components" or "files"
+        function findMessages(messages: object, currentPath: string): MessagePreviewTableProps['data'] {
+            const found: MessagePreviewTableProps['data'] = [];
+            for (const [k, message] of Object.entries(messages)) {
+                if(!(typeof message === 'object')) continue;
+                if ('embeds' in message || 'components' in message || 'files' in message) {
+                    message.key = `${currentPath}.${k}`;
+                    found.push(message);
+                } else {
+                    found.push(...findMessages(message, `${currentPath}.${k}`));
+                }
+            }
+            return found;
+        }
+
+        const data = findMessages(messages, "keys");
+        console.log(data);
+
+        return <div className="App">
+            <MessagePreviewTable data={data}/>
+        </div>
+    }
 }
 
 export default App;
